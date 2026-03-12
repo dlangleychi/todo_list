fd427cedd2f37f4d5d089eb38c9b9bc931ff7d86
Daniel Langley <dlangley812@hotmail.com>
2026-03-12 18:35:21 -0500

    Complete Step 5

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>This is a todo starter!</h1>"
+    return redirect(url_for('get_lists'))
+
+# Render new todo list page
+@app.route("/lists/new")
+def add_todo_list():
+    return render_template('new_list.html')
+
+# Render the list of todo lists
+@app.route("/lists")
+def get_lists():
+    lists = [
+        {"title": "Lunch Groceries", "todos": []},
+        {"title": "Dinner Groceries", "todos": []},
+    ]
+    return render_template('lists.html', lists=lists)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
