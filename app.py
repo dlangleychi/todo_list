a21aac2cab01cd04e02ca5198b8eef743fc291c0
Daniel Langley <dlangley812@hotmail.com>
2026-06-17 17:02:50 -0500

    Do 6.

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def index():
 
 @app.route("/lists")
 def get_lists():
-    lists = sort_items(session['lists'], is_list_completed)
+    lists = sort_items(g.storage.all_lists(), is_list_completed)
     return render_template('lists.html',
                            lists=lists,
                            todos_remaining=todos_remaining)
@@ -79,19 +79,17 @@ def get_lists():
 def create_list():
     title = request.form["list_title"].strip()
 
-    error = error_for_list_title(title, session['lists'])
+    # error = error_for_list_title(title, session['lists'])
+    error = error_for_list_title(title, g.storage.all_lists())
+
     if error:
         flash(error, "error")
         return render_template('new_list.html', title=title)
 
-    session['lists'].append({
-        'id': str(uuid4()),
-        'title': title,
-        'todos': [],
-    })
+    g.storage.create_new_list(title)
 
     flash("The list has been created.", "success")
-    session.modified = True
+
     return redirect(url_for('get_lists'))
 
 @app.route("/lists/new")
@@ -171,7 +169,7 @@ def delete_list(lst, list_id):
 def update_list(lst, list_id):
     title = request.form["list_title"].strip()
 
-    error = error_for_list_title(title, session['lists'])
+    error = error_for_list_title(title, g.storage.all_lists())
     if error:
         flash(error, "error")
         return render_template('edit_list.html', lst=lst, title=title)
--- todos/session_persistence.py
+++ todos/session_persistence.py
@@ -1,8 +1,21 @@
+from uuid import uuid4
+
 class SessionPersistence:
     def __init__(self, session):
         self.session = session
         if 'lists' not in self.session:
             self.session['lists'] = []
+
+    def all_lists(self):
+        return self.session['lists']
+    
+    def create_new_list(self, title):
+        self.session['lists'].append({
+            'id': str(uuid4()),
+            'title': title,
+            'todos': [],
+        })
+        self.session.modified = True
 
     def find_list(self, list_id):
         found = (lst for lst in self.session['lists'] if lst['id'] == list_id)
