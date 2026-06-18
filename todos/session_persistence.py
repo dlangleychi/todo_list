c693920e155ddad8360cf4f78593c9117a1ccac5
Daniel Langley <dlangley812@hotmail.com>
2026-06-17 21:48:39 -0500

    Do 7.

--- todos/session_persistence.py
+++ todos/session_persistence.py
@@ -8,15 +8,27 @@ class SessionPersistence:
 
     def all_lists(self):
         return self.session['lists']
-    
+
+    def find_list(self, list_id):
+        found = (lst for lst in self.session['lists'] if lst['id'] == list_id)
+        return next(found, None)
+
     def create_new_list(self, title):
-        self.session['lists'].append({
+        lists = self.all_lists()
+        lists.append({
             'id': str(uuid4()),
             'title': title,
             'todos': [],
         })
         self.session.modified = True
 
-    def find_list(self, list_id):
-        found = (lst for lst in self.session['lists'] if lst['id'] == list_id)
-        return next(found, None)
+    def update_list_by_id(self, list_id, new_title):
+        lst = self.find_list(list_id)
+        if lst:
+            lst['title'] = new_title
+            self.session.modified = True
+
+    def delete_list(self, list_id):
+        self.session['lists'] = [lst for lst in self.session['lists']
+                                if lst['id'] != list_id]
+        self.session.modified = True
