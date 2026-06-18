4dd33bc03b0fbf6c9cbaf7da5c2014a1c77199e7
Daniel Langley <dlangley812@hotmail.com>
2026-06-18 12:27:09 -0500

    Do 11.

--- todos/database_persistence.py
+++ todos/database_persistence.py
@@ -1,7 +1,13 @@
 from contextlib import contextmanager
 
+import logging
+
 import psycopg2
 from psycopg2.extras import DictCursor
+
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+logger = logging.getLogger(__name__)
 
 class DatabasePersistence:
     def __init__(self):
@@ -17,10 +23,29 @@ class DatabasePersistence:
             connection.close()
 
     def all_lists(self):
-        pass
+        query = "SELECT * FROM lists"
+        logger.info("Executing query: %s", query)
+        with self._database_connect() as conn:
+            with conn.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query)
+                results = cursor.fetchall()
+
+        lists = [dict(result) for result in results]
+        for lst in lists:
+            lst.setdefault('todos', [])
+        
+        return lists
 
     def find_list(self, list_id):
-        pass
+        query = "SELECT * FROM lists WHERE id = %s"
+        logger.info("Executing query: %s with list_id %s", query, list_id)
+        with self._database_connect() as conn:
+            with conn.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query, (list_id,))
+                lst = dict(cursor.fetchone())
+
+        lst.setdefault('todos', [])
+        return lst
 
     def create_new_list(self, title):
         pass
